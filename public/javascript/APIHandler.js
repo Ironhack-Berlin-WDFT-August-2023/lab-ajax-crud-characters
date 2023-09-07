1cdca6d74a4a651efb3869023f2b005c38ae4219
AGUSTINA PATT <agustinapatt@gmail.com>
2023-09-07 16:35:47 +0100

    lab solution

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,28 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+      this.api = axios.create({
+          baseURL: baseUrl
+      })
   }
 
   getFullList () {
+      return this.api.get("/characters")
+  }
+
+  getOneRegister (characterId) {
+      return this.api.get(`/characters/${characterId}`)
+  }
+
+  createOneRegister (characterInfo) {
+      return this.api.post("/characters", characterInfo)
+  }
+
+  updateOneRegister (characterId, characterInfo) {
+      return this.api.put(`/characters/${characterId}`, characterInfo)
 
   }
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister (characterId) {
+      return this.api.delete(`/characters/${characterId}`)
   }
 }
